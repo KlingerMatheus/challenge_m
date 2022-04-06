6b52cb19c436e654c369567de5880939115fe8a0
Klinger <kger.matheus@gmail.com>
2022-04-06 14:26:21 -0300

    feat(navbar): created an responsive navbar to the project
    The Navbar was created being responsive and as a component, it is using the FontAwesome(v 5.14.4)
    dependency for the icons. Also the Navbar is using a CDN font from Google, 'Open Sans'.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  name: "App",
+  components: { Navbar },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap");
+
+/* RESET CSS */
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  border: none;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  font-family: "Open Sans", sans-serif;
+  background-color: rgb(33, 33, 33);
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,163 @@
+<template>
+  <nav id="navbar">
+    <img
+      id="logo"
+      src="../assets/logo-dark.png"
+      alt="Logo Klinger Matheus - Dev Full Stack"
+      height="60px"
+    />
+    <div class="links">
+      <router-link
+        v-for="(link, index) in links"
+        :to="link.path"
+        :key="index"
+        >{{ link.title }}</router-link
+      >
+    </div>
+
+    <div class="links-responsive">
+      <button
+        @click="this.closeResponsiveNavbar"
+        id="close-navbar-responsive"
+        class="icon-button"
+      >
+        <i class="fa fa-times-circle"></i>
+      </button>
+      <router-link
+        v-for="(link, index) in links"
+        :to="link.path"
+        :key="index"
+        >{{ link.title }}</router-link
+      >
+    </div>
+    <div id="toggle-button">
+      <button @click="openResponsiveNavbar" class="icon-button">
+        <i class="fa fa-bars"></i>
+      </button>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data: () => {
+    return {
+      links: [
+        { path: "/", title: "Register User" },
+        { path: "/users", title: "View Users" },
+      ],
+    };
+  },
+  methods: {
+    openResponsiveNavbar() {
+      const responsiveNavbar = document.querySelector(".links-responsive");
+
+      return (responsiveNavbar.style.display = "flex");
+    },
+    closeResponsiveNavbar() {
+      const responsiveNavbar = document.querySelector(".links-responsive");
+
+      return (responsiveNavbar.style.display = "none");
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  transition: cubic-bezier(0.175, 0.885, 0.32, 1.275) 250ms;
+}
+
+#navbar {
+  display: flex;
+  flex-direction: row;
+  background-color: rgb(27, 27, 27);
+  padding-block: 12px;
+  align-items: center;
+  justify-content: space-around;
+}
+
+#navbar a {
+  font-weight: 600;
+  text-decoration: none;
+  margin-inline: 12px;
+  color: #fff;
+  font-size: 24px;
+  padding: 12px 24px;
+  transition: all 250ms;
+}
+
+#navbar a.router-link-exact-active {
+  color: rgb(0, 160, 255);
+  background-color: rgba(0, 160, 255, 0.1);
+  border-radius: 100px;
+}
+
+.icon-button {
+  border-radius: 100%;
+  width: 60px;
+  height: 60px;
+  font-size: 32px;
+  background-color: transparent;
+  color: rgb(0, 160, 255);
+  cursor: pointer;
+}
+
+.icon-button:hover {
+  transform: scale(1.15);
+}
+.icon-button:active {
+  background-color: rgba(0, 160, 255, 0.1);
+}
+
+.links-responsive {
+  display: none;
+  width: 100%;
+  top: 0px;
+  left: 0px;
+  height: 100%;
+  position: fixed;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
+  z-index: 9999;
+}
+
+.links-responsive a {
+  margin-block: 18px;
+}
+
+.links-responsive #close-navbar-responsive {
+  position: fixed;
+  font-size: 48px;
+  bottom: 20px;
+}
+/* RESPONSIVE */
+
+@media screen and (max-width: 900px) {
+  .links {
+    display: none;
+  }
+
+  #toggle-button {
+    display: flex;
+  }
+
+  #logo {
+    max-height: 36px;
+  }
+}
+
+@media screen and (min-width: 901px) {
+  .links {
+    display: flex;
+  }
+
+  #toggle-button {
+    display: none;
+  }
+}
+</style>
